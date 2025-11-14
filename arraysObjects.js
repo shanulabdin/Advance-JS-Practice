c0d9843e3d9693f65174e45885e530a625163158
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-14 18:20:34 +0500

    getEvenNumbers

--- arraysObjects.js
+++ arraysObjects.js
@@ -14,3 +14,15 @@ function shortSumArray(arr){
 }
 console.log(shortSumArray([1, 2, 3, 4])); // 10
 
+
+function getEvenNumbers(arr){
+  let evenNums = []
+
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] % 2 === 0){
+      evenNums.push(arr[i]);
+    }
+  }
+  return evenNums;
+}
+console.log(getEvenNumbers([1, 2, 3, 4, 5, 6])); // [2, 4, 6]
