0bc367f9b5e0d87eec14d916d26f96c92b6774ec
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-14 17:38:29 +0500

    fizzbuzz function

--- script.js
+++ script.js
@@ -17,3 +17,15 @@ function isEven(n){
 }
 
 console.log(isEven(3));
+
+function fizzBuzz(n){
+  if(n % 3 === 0 && n % 5 === 0){
+    return 'FizzBuzz';
+  } else if (n % 3 === 0){
+    return 'Fizz';
+  } else if (n % 5 === 0){
+    return 'Buzz';
+  }
+}
+
+console.log(fizzBuzz(15));
