1e9383857bc9f53e96479c080e303c166120e604
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-15 04:19:53 +0500

    learning objects

---
+++ objects.js
@@ -0,0 +1,9 @@
+const cars = {
+  'toyota' : ['corola', 'vigo', 'chr'], 
+  'honda' : ['civic', 'city', 'hero'], 
+  'suzuki' : ['cultus', 'mehran', 'alto']
+}
+
+console.log(cars);
+console.log(cars.toyota);
+console.log(cars.toyota[1]);
