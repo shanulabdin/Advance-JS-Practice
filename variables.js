4022ac8c5ff36529041bff1cda8d92ac0bcfc05a
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-15 17:06:26 +0500

    learning variables -shaban

---
+++ variables.js
@@ -0,0 +1,25 @@
+const num1 = 10;
+
+let num2 = 20;
+num2 = 15;
+
+const add = num1 + num2;
+
+const sub = num1 - num2;
+
+const num3 = 40
+
+const num4 = 20
+
+const multiply = num3 * num4
+
+
+const num5 = 10
+
+const num6 = 10
+
+const division = num5 / num6
+
+const shaban = add + sub
+console.log (add, sub, shaban);
+
