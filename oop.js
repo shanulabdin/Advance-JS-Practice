4297bceedf70f4c4c268ff7f583c22958dba6e33
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-15 16:29:53 +0500

    leaarn arrays -shaban

--- oop.js
+++ oop.js
@@ -1,14 +1,3 @@
-const totalMarks = 30;
-
-const array = [10, 9, 11];
-console.log(array[2]);
-
-const stringArray = ['shan', 'shaban', 'laiba'];
-console.log(stringArray[0]);
-
-const cars = {
-  'toyota' : 'corola',
-  'honda' : 'city',
-  'suzuki' : 'mehran'
-}
-console.log(cars.toyota);
+const Array = ['banana', 'apple', 'orange']
+console.log(Array)
+console.log(Array[2])
