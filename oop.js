bb436288c587edb80c3516dd5d1c76fa71169d1a
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-15 16:45:49 +0500

    learning objects -shaban

--- oop.js
+++ oop.js
@@ -1,3 +1,15 @@
 const Array = ['banana', 'apple', 'orange']
 console.log(Array)
 console.log(Array[2])
+
+const object = {
+  'totamark' : ['30', '20', '10']
+}
+console.log (object.totamark) 
+
+let object2 = {
+  'toyota' : 2007,
+  'honda' : 2010,
+  'suzuki' : 2015,
+}
+console.log (object2)
