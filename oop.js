7ece46fee6ce00408e804323e9b7c54065319525
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-15 16:22:43 +0500

    teaching objects and arrays

---
+++ oop.js
@@ -0,0 +1,14 @@
+const totalMarks = 30;
+
+const array = [10, 9, 11];
+console.log(array[2]);
+
+const stringArray = ['shan', 'shaban', 'laiba'];
+console.log(stringArray[0]);
+
+const cars = {
+  'toyota' : 'corola',
+  'honda' : 'city',
+  'suzuki' : 'mehran'
+}
+console.log(cars.toyota);
