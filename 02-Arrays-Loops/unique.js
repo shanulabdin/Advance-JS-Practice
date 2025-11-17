8f97efe6fdc0a4dd329bfe0e68335c8609dae061
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-17 10:58:53 +0500

    method for unique(arr)

--- 02-Arrays-Loops/unique.js
+++ 02-Arrays-Loops/unique.js
@@ -2,7 +2,7 @@
 function unique(arr){
   let uniqueArray = [];
   for(let i = 0; i < arr.length; i++){
-    if(!uniqueArray.includes(Number(arr[i]))){
+    if(!uniqueArray.includes(arr[i])){
       uniqueArray.push(arr[i]);
     }
   }
@@ -14,3 +14,13 @@ console.log(unique([1, "1", 1, 2, "2"]));
 console.log(unique([]));
 console.log(unique([5, 5, 5, 5, 5]));
 console.log(unique([1, 2, 3]));
+
+// method unique
+const uniqueMethod = (arr) => [...new Set(arr)];
+
+console.log(uniqueMethod([1, 2, 2, 3, 3, 3]));
+console.log(uniqueMethod(["a", "b", "a", "c", "b"]));
+console.log(uniqueMethod([1, "1", 1, 2, "2"]));
+console.log(uniqueMethod([]));
+console.log(uniqueMethod([5, 5, 5, 5, 5]));
+console.log(uniqueMethod([1, 2, 3]));
