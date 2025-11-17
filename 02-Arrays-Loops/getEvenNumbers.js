c84b21c93f46b7a23535dedb2641d47e9b66eb73
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-17 10:32:29 +0500

    solved with method

--- 02-Arrays-Loops/getEvenNumbers.js
+++ 02-Arrays-Loops/getEvenNumbers.js
@@ -13,3 +13,12 @@ console.log(getEvenNumbers([1, 2, 3, 4, 5, 6]));
 console.log(getEvenNumbers([-3, -2, -1, 0, 1, 2, 3]));
 console.log(getEvenNumbers([1, 3, 5, 7]));
 console.log(getEvenNumbers([2, 4, 6, 8]));
+
+// getEvenNumbers method
+const evenMethod = (arr) => {
+  return arr.filter(curr => curr % 2 === 0)
+}
+console.log(evenMethod([1, 2, 3, 4, 5, 6]));
+console.log(evenMethod([-3, -2, -1, 0, 1, 2, 3]));
+console.log(evenMethod([1, 3, 5, 7]));
+console.log(evenMethod([2, 4, 6, 8]));
