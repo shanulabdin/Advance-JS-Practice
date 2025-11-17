552eb78c4c4371b8e2e78ae66e151f6266dc805a
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-17 11:18:47 +0500

    flatten 2, 3, 4

--- 02-Arrays-Loops/flatten.js
+++ 02-Arrays-Loops/flatten.js
@@ -12,6 +12,7 @@ function flatten(arr){
 }
 
 console.log(flatten([1, [2, 3], [4], 5]));
+console.log(flatten([1, 2, 3]));
+console.log(flatten([[1, 2], [3, 4]]));
+console.log(flatten([1, [], 2, [3, 4], []]));
 
-const nested = [1, [2, 3], [4], 5];
-console.log(Array.isArray(nested[3]))
