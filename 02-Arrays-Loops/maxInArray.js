774386fd9f71ee433f43008a67745f7be0376efa
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-17 10:14:40 +0500

    best practices

--- 02-Arrays-Loops/maxInArray.js
+++ 02-Arrays-Loops/maxInArray.js
@@ -1,16 +1,15 @@
 // maxInArray
 // 1
 function maxInArray(array){
-  if(array[0] === undefined){
-    return null;
-  }
-  let curr = array[0]
+  if(array[0] === undefined) return null;
+
+  let max = array[0];
   for(let i = 1; i < array.length; i++){
-    if(array[i] > curr){
-      curr = array[i];
+    if(array[i] > max){
+      max = array[i];
     }
   }
-  return curr;
+  return max;
 }
 console.log(maxInArray([1, 5, 2, 9, 3]));
 console.log(maxInArray([-10, -5, -20]));
@@ -18,7 +17,7 @@ console.log(maxInArray([3, 7, 7, 2]));
 console.log(maxInArray([]));
 
 const maxMethod = (arr) => {
-  return arr[0] === undefined ? null : Math.max(...arr);
+  return arr.length === 0 ? null : Math.max(...arr);
 }
 console.log(maxMethod([1, 5, 2, 9, 3]));
 console.log(maxMethod([-10, -5, -20]));
