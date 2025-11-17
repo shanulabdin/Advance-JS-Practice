1b9550cf79d8e4bee5abe0e10e9a6a96d3775c57
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-17 10:43:57 +0500

    loop for unique(arr)

---
+++ 02-Arrays-Loops/unique.js
@@ -0,0 +1,16 @@
+// unique
+function unique(arr){
+  let uniqueArray = [];
+  for(let i = 0; i < arr.length; i++){
+    if(!uniqueArray.includes(Number(arr[i]))){
+      uniqueArray.push(arr[i]);
+    }
+  }
+  return uniqueArray;
+}
+console.log(unique([1, 2, 2, 3, 3, 3]));
+console.log(unique(["a", "b", "a", "c", "b"]));
+console.log(unique([1, "1", 1, 2, "2"]));
+console.log(unique([]));
+console.log(unique([5, 5, 5, 5, 5]));
+console.log(unique([1, 2, 3]));
