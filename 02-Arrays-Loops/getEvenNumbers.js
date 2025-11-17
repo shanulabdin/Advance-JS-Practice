c74c99d43d344dc9eece886f87279784d2557173
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-17 10:22:08 +0500

    getEvenNumbers - 2,3,4

--- 02-Arrays-Loops/getEvenNumbers.js
+++ 02-Arrays-Loops/getEvenNumbers.js
@@ -10,3 +10,6 @@ function getEvenNumbers(arr){
   return evenNums;
 }
 console.log(getEvenNumbers([1, 2, 3, 4, 5, 6]));
+console.log(getEvenNumbers([-3, -2, -1, 0, 1, 2, 3]));
+console.log(getEvenNumbers([1, 3, 5, 7]));
+console.log(getEvenNumbers([2, 4, 6, 8]));
