775bc96818e0e5e4b60806b3d804c17f9b1fe55d
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-17 10:06:32 +0500

    maxInArray - 2,3,4

--- 02-Arrays-Loops/maxInArray.js
+++ 02-Arrays-Loops/maxInArray.js
@@ -1,6 +1,9 @@
 // maxInArray
 // 1
 function maxInArray(array){
+  if(array[0] === undefined){
+    return null;
+  }
   let curr = array[0]
   for(let i = 1; i < array.length; i++){
     if(array[i] > curr){
@@ -10,3 +13,6 @@ function maxInArray(array){
   return curr;
 }
 console.log(maxInArray([1, 5, 2, 9, 3]));
+console.log(maxInArray([-10, -5, -20]));
+console.log(maxInArray([3, 7, 7, 2]));
+console.log(maxInArray([]));
