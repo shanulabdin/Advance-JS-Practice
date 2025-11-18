de5e7987dbc4f17aa72caf1d42fb5e30e131e884
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-19 02:43:31 +0500

    correct method of adding delete button

--- 04-dom/todo.js
+++ 04-dom/todo.js
@@ -1,8 +1,6 @@
 const input = document.querySelector('#input');
 const addTaskBtn = document.querySelector('#addTaskBtn');
-const taskTitle = document.querySelector('#taskTitle');
 const taskContainer = document.querySelector('#taskContainer');
-const taskCheck = document.querySelector('.taskCheck');
 
 const delTaskBtns = document.querySelectorAll('.delTaskBtns');
 
@@ -10,12 +8,12 @@ addTaskBtn.addEventListener('click', () => {
   addTask();
 })
 
-delTaskBtns.forEach(delTaskBtn => {
-  delTaskBtn.addEventListener('click', (event) => {
-    const clickedBtn = event.target;
-    const parentBox = clickedBtn.parentNode;
-    parentBox.style.display = 'none';
-  })
+taskContainer.addEventListener('click', (event) => {
+  const clickedItem = event.target;
+  if(clickedItem.classList.value === 'delTaskBtns'){
+    const closestLi = clickedItem.closest('li');
+    closestLi.remove();
+  }
 })
 
 function addTask(){
@@ -26,10 +24,11 @@ function addTask(){
     `<li class="task">
       <div class="taskCheck">
         <input class="taskCheck" type="checkbox">
-        <p class="taskTitle" id="taskTitle">${taskInput}</p>
+        <p class="taskTitle">${taskInput}</p>
       </div>
       <button class="delTaskBtns">-</button>
     </li>`;
     
   input.value = '';
+  input.focus();
 }
