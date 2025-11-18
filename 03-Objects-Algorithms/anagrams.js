f94660e7b3dd6e64eca278af665fc707a1891887
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-18 15:46:42 +0500

    fixed version

--- 03-Objects-Algorithms/anagrams.js
+++ 03-Objects-Algorithms/anagrams.js
@@ -1,30 +1,26 @@
 function isAnagram(word1, word2){
-  let obj = {};
-  let anagram = null;
-
   if(word1.length !== word2.length){
-    anagram = false;
-    return anagram;
+    return false;
   }
 
-  for(let i = 0; i < word1.length; i++){
-    obj[word1[i]] = 1;
+  let freq = {};
+
+  for(let char of word1){
+    if (freq[char] === undefined){
+      freq[char] = 1;
+    } else if (freq[char] === 1){
+      freq[char] += 1;
+    }
   }
-  for(let i = 0; i < word2.length; i++){
-    obj[word2[i]] += 1;
+  
+  for(let char of word2){
+    if(freq[char] === undefined || freq[char] === 0){
+      return false;
+    }
+    freq[char]--;
   }
-  Object.keys(obj).forEach(key => {
-    const value = obj[key];
-    value === 2 ? anagram = true : anagram = false
-  })
-  return anagram;
+  return true;
 }
 
-console.log(isAnagram('listen', 'silent'));
+console.log(isAnagram('listens', 'silents'));
 console.log(isAnagram('car', 'rat'));
-
-// plan:
-// first make an Object --
-// each charcter of word1 will be a key with value 1 --
-// make the charcters of word2 add into the value --
-// check if all the keys have value = 2 ? true : false; --
