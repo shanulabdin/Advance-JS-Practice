ce1d7f8f0d60af1b88231099c1ed525e9c1e7c56
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-19 15:15:30 +0500

    added nice to have timeOut

--- 04-dom/search.js
+++ 04-dom/search.js
@@ -1,13 +1,18 @@
 const inputElement = document.getElementById('input');
 const listItems = document.querySelectorAll('.listItem');
 
+let timeoutId;
 inputElement.addEventListener('input', (event) => {
-  const searchText = event.target.value.trim().toLowerCase();
+  clearTimeout(timeoutId);
 
-  listItems.forEach((listItem) => {
-    const itemText = listItem.textContent.toLowerCase();
-    const isMatch = itemText.includes(searchText);
-
-    listItem.hidden = isMatch ? false : true;
-  })
-})
+  timeoutId = setTimeout(() => {
+    const searchText = event.target.value.trim().toLowerCase();
+    
+    listItems.forEach((listItem) => {
+      const itemText = listItem.textContent.toLowerCase();
+      const isMatch = itemText.includes(searchText);
+      
+      listItem.hidden = isMatch ? false : true;
+    });
+  }, 200);
+});
