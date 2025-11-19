b6a1013a070fd1ce3d127826bca5ffeba2960a05
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-19 14:52:18 +0500

    my first solution

--- 04-dom/search.js
+++ 04-dom/search.js
@@ -1,4 +1,13 @@
-const input = document.getElementById('input');
+const inputElement = document.getElementById('input');
 const listContainer = document.getElementById('listContainer');
 const listItems = document.querySelectorAll('.listItem');
 
+inputElement.addEventListener('input', (event) => {
+  listItems.forEach((listItem) => {
+    if(!listItem.textContent.toLowerCase().includes(event.target.value.toLowerCase())){
+      listItem.style.display = 'none';
+    } else {
+      listItem.style.display = 'block';
+    };
+  })
+})
