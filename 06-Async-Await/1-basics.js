32cba8e5ef81dcc6e8d7576b1f95671f8f4c49d1
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-21 07:11:25 +0500

    2. await pauses execution until a Promise resolves

--- 06-Async-Await/1-basics.js
+++ 06-Async-Await/1-basics.js
@@ -2,3 +2,9 @@ async function myFunc(){
   return 10;
 }
 myFunc().then(result => console.log(result));
+
+async function myFunc(){
+  let result = await Promise.resolve(10);
+  console.log(result);
+}
+myFunc()
