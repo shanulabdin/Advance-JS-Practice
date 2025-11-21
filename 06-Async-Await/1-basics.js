aee59b4be52a921b10f2cd027cea7fba40ea2e5f
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-21 07:17:18 +0500

    3. Using async/await with real async tasks (like API fetch)

--- 06-Async-Await/1-basics.js
+++ 06-Async-Await/1-basics.js
@@ -1,11 +1,22 @@
+// 1
 async function myFunc(){
   return 10;
 }
 myFunc().then(result => console.log(result));
 
+// 2
 async function getNumber() {
   let result = await Promise.resolve(20);
   console.log(result); // 20
 }
 
 getNumber();
+
+// 3
+async function getUser() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users/1');
+  const user = await res.json();
+  console.log(user);
+}
+getUser();
+
