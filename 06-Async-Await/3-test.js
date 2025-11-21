ea021f66ea1a007d0d7178b56ef9335e258be751
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-21 09:37:01 +0500

    better version

--- 06-Async-Await/3-test.js
+++ 06-Async-Await/3-test.js
@@ -1,23 +1,11 @@
 async function getPosts(){
-  try {
-    const urls = [
-      `https://jsonplaceholder.typicode.com/posts/1`,
-      `https://jsonplaceholder.typicode.com/posts/2`,
-      `https://jsonplaceholder.typicode.com/posts/3`,
-      `https://jsonplaceholder.typicode.com/posts/4`,
-      `https://jsonplaceholder.typicode.com/posts/5`,
-      `https://jsonplaceholder.typicode.com/posts/6`,
-      `https://jsonplaceholder.typicode.com/posts/7`,
-      `https://jsonplaceholder.typicode.com/posts/8`,
-      `https://jsonplaceholder.typicode.com/posts/9`,
-      `https://jsonplaceholder.typicode.com/posts/10`
-    ]
+  const url = `https://jsonplaceholder.typicode.com/posts`
 
-    const responses = await Promise.all(urls.map(url => fetch(url)));
-    const posts = await Promise.all(responses.map(post => post.json()));
-    console.log(posts);
-  } catch (err){
-    console.log('something went wrong: ', err)
-  }
+  const res = await fetch(url);
+  const posts = await res.json();
+
+  console.log(posts.slice(0, 10));
 }
-getPosts();
+getPosts().catch(err => {
+  console.log('Something went wrong: ', err)
+});
