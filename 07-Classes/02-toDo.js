eb4113cb5bb04da5aa695f3508f392b1f7ed94c5
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-21 19:04:04 +0500

    learning default parameters

--- 07-Classes/02-toDo.js
+++ 07-Classes/02-toDo.js
@@ -1,5 +1,5 @@
-class TodoItems {
-  constructor(task, completed){
+class TodoItem {
+  constructor(task = 'New Task', completed = false){
     this.task = task;
     this.completed = completed;
   }
@@ -11,12 +11,17 @@ class TodoItems {
   }
 }
 
-const task1 = new TodoItems('Buy milk', true);
+const task1 = new TodoItem('Buy Milk', true);
 console.log(task1.toString());
 task1.toggle();
 console.log(task1.toString());
 
-const task2 = new TodoItems('Workout', false);
+const task2 = new TodoItem('Workout', false);
 console.log(task2.toString());
 task2.toggle();
 console.log(task2.toString());
+
+const task3 = new TodoItem();
+console.log(task3.toString());
+task3.toggle();
+console.log(task3.toString());
