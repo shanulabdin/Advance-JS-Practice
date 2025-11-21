73e36467dd9ab704fb841ee8569f660ab8dc719e
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-21 10:40:18 +0500

    added loading....

--- 06-Async-Await/3-test.js
+++ 06-Async-Await/3-test.js
@@ -4,24 +4,31 @@ const getPostsBtn = document.querySelector('.getPostsBtn');
 async function getPosts(){
   const url = `https://jsonplaceholder.typicode.com/posts`
 
+  showPosts.innerHTML = `<p>loading...</p>`;
+  
   try{
     const res = await fetch(url);
     const posts = await res.json();
-
-    const tenPosts = posts.slice(0, 10);
-    const postTitles = tenPosts.map(p => p.title);
     
-    for(let i = 0; i < postTitles.length; i++){
-      const li = document.createElement('li');
-      li.textContent = `
+    setTimeout(() => {
+      const tenPosts = posts.slice(0, 10);
+      const postTitles = tenPosts.map(p => p.title);
+      
+      showPosts.innerHTML = `<p></p>`;
+      
+      for(let i = 0; i < postTitles.length; i++){
+        const li = document.createElement('li');
+        li.textContent = `
         ${postTitles[i]}
-      `;
-      showPosts.appendChild(li);
-    }
-    
+        `;
+        showPosts.appendChild(li);
+      }
+    }, 1000);
   } catch (err) {
     showPosts.innerHTML = `<p>Something went wrong: ${err}</p>`
     console.log('Something went wrong: ', err);
   }
 }
-getPostsBtn.addEventListener('click', () => getPosts());
+getPostsBtn.addEventListener('click', () => {
+  getPosts();
+});
