e1a3ac3bd9a593bf36bdbd74b76abb0c0c6307b1
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-22 12:41:57 +0500

    taking input and putting it in dom

---
+++ 07-Classes/04-DOM.js
@@ -0,0 +1,20 @@
+const taskInput = document.querySelector('#taskInput');
+const addTaskBtn = document.querySelector('#addTaskBtn');
+const taskContainer = document.querySelector('#taskContainer');
+
+
+
+addTaskBtn.addEventListener('click', () => {
+  const inputText = taskInput.value;
+
+  taskContainer.innerHTML += `
+    <li class="task">
+      <div class="itemContainer">
+        <input class="taskCheck" type="checkbox">
+        <p class="taskTitle">${inputText}</p>
+      </div>
+      <button class="delTaskBtns">-</button>
+    </li>
+  `;
+})
+
