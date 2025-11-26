92e84f6f83443ff510244362c18e6d54ab3f534a
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-26 14:44:36 +0500

    setting up

--- 08-Quiz-App-prj1/script.js
+++ 08-Quiz-App-prj1/script.js
@@ -1,19 +1,18 @@
 const newQuestion = document.querySelector('.newQuestion');
 const nextButton = document.querySelector('#nextButton');
 
-nextButton.addEventListener('click', nextQuestion);
-
 function nextQuestion(){
   newQuestion.innerHTML = `
-  <div id="questionContainer">
-  <p class="question">What is the biggest city in the world.</p>
-  </div>
-  
-  <div id="optionContainer">
-  <button class="answerButton">Karachi</button>
-  <button class="answerButton">Tokyo</button>
-  <button class="answerButton">Delhi</button>
-  <button class="answerButton">New York</button>
-  </div>
+    <div id="questionContainer">
+    <p class="question">What is the biggest city in the world.</p>
+    </div>
+    
+    <div id="optionContainer">
+    <button class="answerButton">Karachi</button>
+    <button class="answerButton">Tokyo</button>
+    <button class="answerButton">Delhi</button>
+    <button class="answerButton">New York</button>
+    </div>
   `;
 }
+nextButton.addEventListener('click', nextQuestion);
