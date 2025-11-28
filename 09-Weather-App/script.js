3bdc4bd2aef64846222b6dabf4bfb4d03a5a92cd
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-28 11:17:52 +0500

    getting city coordinates

--- 09-Weather-App/script.js
+++ 09-Weather-App/script.js
@@ -6,18 +6,28 @@ const weekday = document.querySelectorAll('.weekday');
 
 
 const API_KEY = `2169d28cbb315f3b55d6b51417fbcd38`;
-const lat = 24.8607;
-const lon = 67.0011;
+let lat = 24.8607;
+let lon = 67.0011;
+let city = '';
+const limit = 5;
 
-async function getWeather() {
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+async function getCityCoor() {
+  const underInput = getCityName.value.trim();
+  city = underInput.charAt(0).toUpperCase() + underInput.slice(1);
 
-  const response = await fetch(url);
-  if(!response.ok){
-    throw new Error('Something went wrong:', response.status)
-  }
-  const data = await response.json();
-  console.log(data);
+  const url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+  lat = data[0].lat;
+  lon = data[0].lon;
+
+  getCityName.value = '';
+  getCityName.focus();
 }
-
-getWeatherBtn.addEventListener('click', getWeather);
+getWeatherBtn.addEventListener('click', getCityCoor);
+getCityName.addEventListener('keydown', (e) => {
+  if(e.key === 'Enter'){
+    getCityCoor();
+  }
+});
