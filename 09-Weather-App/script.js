de9f1002cef13ae3acf125deecfd533cb0aadc83
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-28 09:59:37 +0500

    get input

--- 09-Weather-App/script.js
+++ 09-Weather-App/script.js
@@ -1,4 +1,11 @@
+const getCityName = document.querySelector('.getCityName');
+const getWeatherBtn = document.querySelector('.getWeatherBtn');
 const cityName = document.querySelector('#cityName');
 const weatherNow = document.querySelector('.weatherNow');
 const weekday = document.querySelectorAll('.weekday');
 
+
+function getWeather(){
+  const input = getCityName.value.trim();
+}
+getWeatherBtn.addEventListener('click', getWeather)
