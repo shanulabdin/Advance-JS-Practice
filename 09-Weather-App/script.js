e707e169e506b66ed930363cc9ea92f2ab8f26f7
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-28 10:41:26 +0500

    getting the weather

--- 09-Weather-App/script.js
+++ 09-Weather-App/script.js
@@ -5,7 +5,19 @@ const weatherNow = document.querySelector('.weatherNow');
 const weekday = document.querySelectorAll('.weekday');
 
 
-function getWeather(){
-  const input = getCityName.value.trim();
+const API_KEY = `2169d28cbb315f3b55d6b51417fbcd38`;
+const lat = 24.8607;
+const lon = 67.0011;
+
+async function getWeather() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+  const response = await fetch(url);
+  if(!response.ok){
+    throw new Error('Something went wrong:', response.status)
+  }
+  const data = await response.json();
+  console.log(data);
 }
-getWeatherBtn.addEventListener('click', getWeather)
+
+getWeatherBtn.addEventListener('click', getWeather);
