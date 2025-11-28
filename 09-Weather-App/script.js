7cc8e672a10fb512a5ee45c79e9a8006c8af8ef7
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-28 13:11:46 +0500

    adding error handling and better code

--- 09-Weather-App/script.js
+++ 09-Weather-App/script.js
@@ -1,40 +1,38 @@
 const getCityName = document.querySelector('.getCityName');
 const getWeatherBtn = document.querySelector('.getWeatherBtn');
 const showWeather = document.querySelector('.showWeather');
+const showError = document.querySelector('.showError');
+const error = document.querySelector('#error');
 
+const cityName = document.querySelector('#cityName');
+const tempNow = document.querySelector('#tempNow');
 const feelsLike = document.querySelector('#feelsLike');
 const weatherDes = document.querySelector('#weatherDes');
-const tempNow = document.querySelector('#tempNow');
 
 
 const API_KEY = `2169d28cbb315f3b55d6b51417fbcd38`;
-let lat = 24.8607;
-let lon = 67.0011;
 let city = 'Karchi';
-const limit = 5;
-
-async function getCityCoor() {
-  const underInput = getCityName.value.trim();
-  city = underInput.charAt(0).toUpperCase() + underInput.slice(1);
-
-  const url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
-
-  const res = await fetch(url);
-  const data = await res.json();
-  lat = data[0].lat;
-  lon = data[0].lon;
-
-  getCityName.value = '';
-  getCityName.focus();
-}
 
 async function getWeather() {
+  const underInput = getCityName.value.trim();
+  city = underInput.charAt(0).toUpperCase() + underInput.slice(1);
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
-  const res = await fetch(url);
-  const data = await res.json();
-
-  renderWeather(data);
+  
+  showError.style.display = 'none';
+  try{
+    const res = await fetch(url);
+    const data = await res.json();
+    
+    renderWeather(data);
+  } catch (err){
+    error.innerHTML = err;
+    showError.style.display = 'block';
+    showWeather.style.display = 'none';
+  } finally {
+    getCityName.value = '';
+    getCityName.focus();
+  }
 }
 
 function renderWeather(data){
@@ -47,12 +45,10 @@ function renderWeather(data){
 }
 
 getWeatherBtn.addEventListener('click', async function() {
-  await getCityCoor();
   await getWeather();
 });
 getCityName.addEventListener('keydown', async function(e){
   if(e.key === 'Enter'){
-    await getCityCoor();
     await getWeather();
   }
 });
